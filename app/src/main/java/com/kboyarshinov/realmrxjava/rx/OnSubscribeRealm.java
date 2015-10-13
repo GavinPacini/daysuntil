8720673089db4c39281795fbb3a46380c424ef80
GavinThePacMan <gavin.pacini@gmail.com>
2015-10-13 22:33:13 +0100

    Updated to use newer OnSubscribeRealm as per issue #1

--- app/src/main/java/com/kboyarshinov/realmrxjava/rx/OnSubscribeRealm.java
+++ app/src/main/java/com/kboyarshinov/realmrxjava/rx/OnSubscribeRealm.java
@@ -1,19 +1,28 @@
 package com.kboyarshinov.realmrxjava.rx;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.exceptions.RealmException;
 import rx.Observable;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.functions.Action0;
 import rx.subscriptions.Subscriptions;
 
 abstract class OnSubscribeRealm<T> implements Observable.OnSubscribe<T> {
-    private Context context;
-    private String fileName;
+    private final Context context;
+    private final String fileName;
+
+    private final List<Subscriber<? super T>> subscribers = new ArrayList<>();
+    private final AtomicBoolean canceled = new AtomicBoolean();
+    private final Object lock = new Object();
 
     public OnSubscribeRealm(Context context) {
         this(context, null);
@@ -24,66 +33,98 @@ abstract class OnSubscribeRealm<T> implements Observable.OnSubscribe<T> {
         this.fileName = fileName;
     }
 
-    @Nullable
-    private Thread thread;
-
     @Override
     public void call(final Subscriber<? super T> subscriber) {
-        thread = Thread.currentThread();
-        subscriber.add(Subscriptions.create(new Action0() {
-            @Override
-            public void call() {
-                if (thread != null && !thread.isInterrupted()) {
-                    thread.interrupt();
-                }
+        synchronized (lock) {
+            boolean canceled = this.canceled.get();
+            if (!canceled && !subscribers.isEmpty()) {
+                subscriber.add(newUnsubscribeAction(subscriber));
+                subscribers.add(subscriber);
+                return;
+            } else if (canceled) {
+                return;
             }
-        }));
+        }
+        subscriber.add(newUnsubscribeAction(subscriber));
+        subscribers.add(subscriber);
 
         RealmConfiguration.Builder builder = new RealmConfiguration.Builder(context);
         if (fileName != null) {
             builder.name(fileName);
         }
         Realm realm = Realm.getInstance(builder.build());
-        boolean interrupted = false;
         boolean withError = false;
 
         T object = null;
         try {
-            realm.beginTransaction();
-            object = get(realm);
-            interrupted = thread.isInterrupted();
-            if (object != null && !interrupted) {
-                realm.commitTransaction();
-            } else {
-                realm.cancelTransaction();
+            if (!this.canceled.get()) {
+                realm.beginTransaction();
+                object = get(realm);
+                if (object != null && !this.canceled.get()) {
+                    realm.commitTransaction();
+                } else {
+                    realm.cancelTransaction();
+                }
             }
         } catch (RuntimeException e) {
             realm.cancelTransaction();
-            e.printStackTrace();
-            subscriber.onError(new RealmException("Error during transaction.", e));
+            sendOnError(new RealmException("Error during transaction.", e));
             withError = true;
         } catch (Error e) {
             realm.cancelTransaction();
-            subscriber.onError(e);
+            sendOnError(e);
             withError = true;
         }
-        if (object != null && !interrupted && !withError) {
-            subscriber.onNext(object);
+        if (object != null && !this.canceled.get() && !withError) {
+            sendOnNext(object);
         }
 
         try {
             realm.close();
         } catch (RealmException ex) {
-            subscriber.onError(ex);
+            sendOnError(ex);
             withError = true;
         }
-        thread = null;
         if (!withError) {
+            sendOnCompleted();
+        }
+        this.canceled.set(false);
+    }
+
+    private void sendOnNext(T object) {
+        for (int i = 0; i < subscribers.size(); i++) {
+            Subscriber<? super T> subscriber = subscribers.get(i);
+            subscriber.onNext(object);
+        }
+    }
+
+    private void sendOnError(Throwable e) {
+        for (int i = 0; i < subscribers.size(); i++) {
+            Subscriber<? super T> subscriber = subscribers.get(i);
+            subscriber.onError(e);
+        }
+    }
+
+    private void sendOnCompleted() {
+        for (int i = 0; i < subscribers.size(); i++) {
+            Subscriber<? super T> subscriber = subscribers.get(i);
             subscriber.onCompleted();
         }
-        if (interrupted) {
-            Thread.currentThread().interrupt();
-        }
+    }
+
+    @NonNull
+    private Subscription newUnsubscribeAction(final Subscriber<? super T> subscriber) {
+        return Subscriptions.create(new Action0() {
+            @Override
+            public void call() {
+                synchronized (lock) {
+                    subscribers.remove(subscriber);
+                    if (subscribers.isEmpty()) {
+                        canceled.set(true);
+                    }
+                }
+            }
+        });
     }
 
     public abstract T get(Realm realm);
